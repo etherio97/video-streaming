6345898e0961ad43d729407e0dd1331514923f34
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-06-26 03:14:26 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const input = process.argv[2];
-const { sync } = require('glob');
-
-sync(__dirname + '/*.mp4').forEach(file => {
-  console.log(file);
-})
+!async function() {
+  const input = process.argv[2];
+  const { sync } = require('glob');
+  const { default: axios } = require('axios');
+  const firebaseConfig = (await axios('https://workflows-test-13c0a.web.app/__/firebase/init.json')).data;
+  const videos = sync(__dirname + '/*.mp4');
+  console.log(firebaseConfig, videos);
+}().catch(e => {
+  console.error("[ERROR]", e, e.message);
+});
